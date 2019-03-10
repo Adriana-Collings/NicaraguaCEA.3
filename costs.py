3c39bf244da447aad358e29227b7128e749e93f3
Adriana <adriana.collings@yale.edu>
2019-03-09 18:56:25 -0700

    conditions progress

--- costs.py
+++ costs.py
@@ -1,22 +1,6 @@
-import SimParameters as SP
+Both_1yrT = 60157.50
 
-
-infrastructure = 189190.15
-equipment_donation = 327868.66
-acquired_equipment = 39126.21
-equipment_supplies = 288742.45
-education_training = float(58175) + float(67961)
-
-print(infrastructure)
-print(equipment_donation)
-print(acquired_equipment)
-print(equipment_supplies)
-print(education_training)
-
-
-total = float(infrastructure) + float(equipment_donation) + float(acquired_equipment) + float(equipment_supplies) + float(education_training)
-
-OpSmile_C = total                    # Cost of Operation Smile Project Implementation
+OpSmile_C = Both_1yrT                   # Cost of Operation Smile Project Implementation
 OS_Surgery_C = 0                # Cost of Surgery with OpSmile
 OS_NoSurgery_C = 0                  # Cost of No surgery with OpSmile
 #NoOpSmile
@@ -24,4 +8,3 @@ NoOS_C = 0                         # Cost of No Operation Smile Project Implemen
 NoOS_Surgery_C = 0                # Cost of NoOpSmile Surgery
 NoOS_NoSurgery_C = 0               # Cost no surgery without OpSmile
 
-print(total)
